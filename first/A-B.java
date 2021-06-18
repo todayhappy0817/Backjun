dba35a8744fd86148a3931e34a5c9bb47fc58114
todayhappy0817 <80230579+todayhappy0817@users.noreply.github.com>
2021-06-18 17:39:58 +0900

    Create A-B.java

---
+++ first/A-B.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bs = new BufferedWriter(new OutputStreamWriter(System.out));
+		try {
+			String Min = bf.readLine();	
+			StringTokenizer st = new StringTokenizer(Min, " ");
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			bs.write(String.valueOf(A-B));
+			bs.flush();
+			bs.close();
+			bf.close();
+		}
+		catch(IOException e) {
+			System.out.println("Waring");
+		}
+	}
+}
