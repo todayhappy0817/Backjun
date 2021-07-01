4babbea86177378dc8de9d96c4d12c296ead513f
todayhappy0817 <80230579+todayhappy0817@users.noreply.github.com>
2021-07-01 17:30:25 +0900

    Create 별찍기2.java

---
+++ third/별찍기2.java
@@ -0,0 +1,30 @@
+package Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb =new StringBuilder();
+		int i,j;
+		try {
+			int count = Integer.parseInt(br.readLine());
+			for(i=1; i<=count; i++) {
+				
+				for(j=0; j<count-i; j++)
+					sb.append(" ");
+				for(j=1; j<=i; j++) 
+					sb.append("*");
+				sb.append("\n");
+			}
+			System.out.println(sb);
+			br.close();
+			
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
